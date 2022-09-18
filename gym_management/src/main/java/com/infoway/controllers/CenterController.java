4c52077e4b7358135149fd11c41063c877c0ecb6
pavan bhujbal <pavanbhujbal18@gmail.com>
2022-09-18 23:03:05 +0530

    date 18 gym and gym_management added center api

---
+++ gym_management/src/main/java/com/infoway/controllers/CenterController.java
@@ -0,0 +1,63 @@
+package com.infoway.controllers;
+import java.util.List;
+
+import com.infoway.services.CenterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.infoway.models.entities.Center;
+
+import Exceptions.CenterNotFoundException;
+
+
+@CrossOrigin
+@RestController
+public class CenterController {
+
+	@Autowired
+	private CenterService centerService;
+	
+	@PostMapping("/centers")
+	Center newCenter(@RequestBody Center newCenter)
+	{
+		return centerService.save(newCenter);
+	}
+	
+	
+	@GetMapping("/centers")
+	List<Center> getAllCenters()
+	{
+		return centerService.findAll();
+	}
+
+	@GetMapping("/my_centers")
+	List<Center> getOwnerCenters(@RequestParam(name = "owner_id") Integer ownerId) {
+		return centerService.findMyCenters(ownerId);
+	}
+
+	@GetMapping("/all_centers")
+	List<Center> getAllFilteredCenters(@RequestParam(name = "locality") String locality) {
+		return centerService.findByLocality(locality);
+	}
+
+	@PutMapping("/centers/{id}")
+	Center updateCenter(@RequestBody Center newCenter, @PathVariable Integer id) {
+		Center center = centerService.findById(id);
+		center.setName(newCenter.getName());
+		center.setDescription(newCenter.getDescription());
+		center.setLocality(newCenter.getLocality());
+		center.setAddress(newCenter.getAddress());
+		center.setContact(newCenter.getContact());
+		center.setCreated_at(newCenter.getCreated_at());
+		center.setUpdated_at(newCenter.getUpdated_at());
+		return centerService.save(center);
+	}
+
+	//delete user
+	@DeleteMapping("/centers/{id}")
+	public void delete(@PathVariable("id") int centerId){
+		centerService.deleteById(centerId);
+	}
+
+
+}
